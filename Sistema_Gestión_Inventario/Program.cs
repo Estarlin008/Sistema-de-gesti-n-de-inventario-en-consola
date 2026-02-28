e2716cbf683f0284c7df774e27ed350e0037c27d
Estarlin Mercedes <estarlinmercedes123@gmail.com>
2026-02-27 23:52:52 -0400

    feat: Creación de la función de ventas.

--- Sistema_Gestión_Inventario/Program.cs
+++ Sistema_Gestión_Inventario/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Data;
 using System.Diagnostics.Contracts;
 
 namespace Sistema_Gestión_Inventario;
@@ -8,56 +9,123 @@ class Progran
     static List<Producto> ListProducto = new List<Producto>();
     static void Main(string[] args)
     {
-        bool salir = false;
-        while (!salir)
+        try
         {
-            Console.WriteLine("------------Menu---------------");
-            Console.WriteLine("1. Ingresar productos");
-            Console.WriteLine("2. Vender Producto");
-            Console.WriteLine("3. Ver Productos en el inventario");
-            Console.WriteLine("4. Abastecer producto");
-            Console.WriteLine("5. Eliminar producto");
-            Console.WriteLine("6. Salir");
 
-            string imput = Console.ReadLine();
-
-            if(int.TryParse(imput, out int opcion))
+            bool salir = false;
+            while (!salir)
             {
-                switch (opcion)
+                Console.WriteLine("------------Menu---------------");
+                Console.WriteLine("1. Ingresar productos");
+                Console.WriteLine("2. Vender Producto");
+                Console.WriteLine("3. Ver Productos en el inventario");
+                Console.WriteLine("4. Abastecer producto");
+                Console.WriteLine("5. Eliminar producto");
+                Console.WriteLine("6. Salir");
+
+                string imput = Console.ReadLine();
+
+                if(int.TryParse(imput, out int opcion))
                 {
-                    
-                }
-            }
-            else
-            {
-                Console.WriteLine("Debe de ingresar un numero");
-            }
+                    switch (opcion)
+                    {
+                        case 1:
+                        AgregarProducto(ListProducto);
+                        break;
 
-            if (!salir)
-            {
-                Console.WriteLine("/nPrecione S para salir del programa");
-                Console.ReadKey();
-            }
+                        case 2:
+                        VenderProducto(ListProducto);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Debe de ingresar un numero");
+                }
+
+                if (!salir)
+                {
+                    Console.WriteLine("/nPrecione S para salir del programa");
+                    Console.ReadKey();
+                }
+        }
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 
 
-    public void AgregarProducto(List<Producto> productos)
+    public static void AgregarProducto(List<Producto> productos)
     {
         Console.WriteLine("Ingrese el Nombre del producto");
         string nombre = Console.ReadLine();
 
+        foreach(Producto prod in productos)
+        {
+            if(prod.Nombre.ToLower() == nombre.ToLower())
+            {
+                Console.WriteLine("El Producto ya existe");
+                return;
+            }
+        }
+
         Console.WriteLine("Ingrese el precio");
-        decimal precio = Convert.ToDecimal(Console.ReadLine());
+       if(!decimal.TryParse(Console.ReadLine(), out decimal precio))
+        {
+            Console.WriteLine("Precio invalido");
+            return;
+        }
 
         Console.WriteLine("Ingrese la cantidad");
-        int cantidad = Convert.ToInt32(Console.ReadLine());
+        if(!int.TryParse(Console.ReadLine(), out int cantidad))
+        {
+            Console.WriteLine("Cantidad invalidad");
+            return;
+        }
 
         Producto nuevo = new Producto(
         nombre,
         precio,
         cantidad
         );
+
+        productos.Add(nuevo);
+        Console.WriteLine("Producto registrado correctamente");
+    }
+
+    public static void VenderProducto(List<Producto> productos)
+    {
+        bool encontrado = false;
+        Console.WriteLine("Ingrese el nombre del producto");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la cantidad del producto");
+        if(!int.TryParse(Console.ReadLine(), out int cantidad))
+        {
+            Console.WriteLine("Cantidad invalidad");
+            return;
+        }
+
+        foreach(Producto p in productos)
+        {
+            if(p.Nombre.ToLower() == nombre.ToLower())
+            {
+                if(p.Cantidad >= cantidad)
+                {
+                    p.Cantidad -= cantidad;
+                    Console.WriteLine("Venta valida");
+                }
+                encontrado = true;
+                break;
+            }
+
+        }
+
+        if (!encontrado)
+        {
+            Console.WriteLine("Producto no encontrado");
+        }        
     }
 
 }
